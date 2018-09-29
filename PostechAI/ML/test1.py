1ba9016b7cca56c88cb152b1d01e549a49bac435
glee1228 <glee1228@users.noreply.github.com>
2018-09-29 20:36:17 +0900

    add Mutate sentence

--- PostechAI/ML/test1.py
+++ PostechAI/ML/test1.py
@@ -108,9 +108,44 @@ def fibo_dynamic(n):
 
     return fibo_r(n)
 
+
 def mutateSentences(sentence):
-    
-    return
+    """
+    High-level idea: generate sentences similar to a given sentence.
+    Given a sentence (sequence of words), return a list of all possible
+    alternative sentences of the same length, where each pair of adjacent words
+    also occurs in the original sentence. (The words within each pair should appear
+    in the same order in the output sentence as they did in the orignal sentence.)
+    Notes:
+    - The order of the sentences you output doesn't matter.
+    - You must not output duplicates.
+    - Your generated sentence can use a word in the original sentence more than
+      once.
+    """
+    # BEGIN_YOUR_CODE
+    input_words = sentence.split()
+    word_set = set(input_words)
+    next_word_set_dict = {word: set() for word in word_set}
+
+    for i in range(len(input_words) - 1):
+        next_word_set_dict[input_words[i]].add(input_words[i + 1])
+
+    sentences = []
+    stack = []
+
+    def mutate_sentence_r(word):
+        stack.append(_X_)  # push something
+        if len(_X_) == len(_X_):
+            sentences.append(' '.join(stack))
+        else:
+            for next_word in next_word_set_dict[word]:
+                mutate_sentence_r(_X_)
+        stack.pop()
+
+    for word in word_set:
+        mutate_sentence_r(word)
+
+    return sentences
 
 def main():
     #print(computeMaxWordLength1("cat dog manipulation"))
